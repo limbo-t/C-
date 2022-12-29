05f841914617452e086eca24efc07bd101a3144e
limbo-t <2324899904@qq.com>
2022-12-29 13:00:45 +0800

    add

--- C代码缓冲区/C代码缓冲区.c
+++ C代码缓冲区/C代码缓冲区.c
@@ -1,29 +1,101 @@
 #include<stdio.h>
-#include<stdlib.h>
-
-int total = 0;
-
-int fibonacci(int num,int *f)
+int ans[50][50] = { 0 };
+char txt[50][50];
+int count = 0;
+void backtrack(int n, int m, int x, int y)
 {
-	total += 1;
-	if (num == 1 || num == 2)
-		return f[num] = 1;
-	if (f[num] == 0)
-		return f[num] = fibonacci(num - 1,f) + fibonacci(num - 2,f);
+	ans[x][y] = 1;
+	if (x + 1 < m)
+	{
+		if (txt[x + 1][y] == '0'&&ans[x+1][y]==0)
+			backtrack(n, m, x + 1, y);
+	}
+	if (x - 1 >= 0)
+	{
+		if (txt[x - 1][y] == '0' && ans[x -1][y] == 0)
+			backtrack(n, m, x - 1, y);
+	}
+	if (y + 1 < n)
+	{
+		if (txt[x][y + 1] == '0' && ans[x][y+1] == 0)
+			backtrack(n, m, x, y + 1);
+	}
+	if (y - 1 >= 0)
+	{
+		if (txt[x][y - 1] == '0' && ans[x][y-1] == 0)
+			backtrack(n, m, x, y - 1);
+	}
+	return;
+}
+char judge(int n, int m)
+{
+	for (int j = 0; j < n; j++)
+	{
+		for (int k = 0; k < m; k++)
+			if (txt[j][k] == '0')
+			{
+				count++;
+				backtrack(n, m, j, k);
+				goto A;
+			}
+	}
+	A:
+	for (int j = 0; j < n; j++)
+	{
+		for (int k = 0; k < m; k++)
+		{
+			if (ans[j][k] == 0 && txt[j][k] == '0')
+			{
+				count++;
+				backtrack(n, m, j, k);
+				goto B;
+			}
+		}
+	}
+	B:
+	for (int j = 0; j < n; j++)
+	{
+		for (int k = 0; k < m; k++)
+		{
+			if (ans[j][k] == 0 && txt[j][k] == '0')
+			{
+				count++;
+				backtrack(n, m, j, k);
+				goto C;
+			}
+		}
+	}
+	C:
+	if (count == 1)
+		return 'C';
+	else if (count == 2)
+		return 'A';
 	else
-		return f[num];
+		return 'B';
 }
 
 int main()
 {
-	int num;
-	scanf("%d", &num);
-	int* f=(int*)malloc((num+1) * 4);
-	memset(f, 0, (num+1) * 4);
-	fibonacci(num,f);
-	for (int i = 1; i <= num; i++)
-		printf("%d ", f[i]);
-	printf("\ntotal=%d\n", total);
-	free(f);
+	int t,n,m;
+	scanf("%d", &t);
+	for (int i = 0; i < t; i++)
+	{
+		scanf("%d %d", &n, &m);
+		rewind(stdin);
+		for (int j = 0; j < n; j++)
+		{
+			for (int k = 0; k < m; k++)
+			{
+				scanf("%c", &txt[j][k]);
+			}
+			char c = getchar();
+		}
+		count = 0;
+		for (int j = 0; j < 50; j++)
+			for (int k = 0; k < 50; k++)
+				ans[j][k] = 0;
+
+		printf("%c\n", judge(n, m));
+	}
 	return 0;
 }
